57f804b6493982fd029159d1c6a6e7cb36fcbc0f
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-01-24 00:21:35 +0530

    1st project added(The quizz game)

---
+++ Quizz game/main.py
@@ -0,0 +1,39 @@
+print("Welcome to my quizz game! ")
+playing = input("Do you want to play this game: ")
+if playing.lower() != "yes":
+    print("Ok... Thank you! Bye")
+    quit()
+
+print("Welcome to my quizz game!")
+score = 0
+
+answer = input("What does CPU stand for: ")
+if answer.lower() == "central processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does GPU stand for: ")
+if answer.lower() == "graphic processing unit":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does RAM stand for: ")
+if answer.lower() == "random access memory":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does PSU stand for: ")
+if answer.lower() == "power supply unit":
+    print("correct!")
+    score += 1
+else:
+    print("Incorrect")
+
+print("You got " + str(score) + "/4")
+print('You got ' + str((score / 4) * 100) + "%")
